2f1714408194e3c170699cb4600e39aa03fcd7d0
robotkutya <mihalyfuredi@gmail.com>
2022-12-08 00:46:23 +0100

    chore: adds tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://www.mihaly.dev',
-	integrations: [mdx(), sitemap()],
-	server: {
-		port: 1337
-	}
+  site: 'https://www.mihaly.dev',
+  integrations: [mdx(), sitemap(), tailwind()],
+  server: {
+    port: 1337
+  }
 });
