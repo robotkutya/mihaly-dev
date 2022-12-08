e18b8e5bc83b7578bce5719196ce446b96062f87
robotkutya <mihalyfuredi@gmail.com>
2022-12-08 01:30:02 +0100

    chore: adds design tokens to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+					'primary-black': '#181818',
+					'primary-gray': '#CFCFCF',
+					'primary-green': '#00BB56',
+					'primary-blue': '#0029FF',
+					'primary-yellow': '#EBA900'
+			},
+			fontFamily: {
+				'silkscreen': ['Silkscreen', 'ui-sans-serif', 'sans'],
+				'inter': ['Inter', 'ui-sans-serif', 'sans']
+			}
 	},
 	plugins: [],
+	},
 }
