2c723eb250e267a65047622e345a471f9765d5b2
Mihaly Furedi <mihaly@mihaly.dev>
2026-06-09 11:59:42 +0200

    updates dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.mihaly.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
   server: {
     port: 1337
+  },
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
